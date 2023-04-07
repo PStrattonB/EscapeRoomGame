76e8f8289d1b7f55182adebde12201e7c93402e2
strat <sorahbinc@gmail.com>
2023-04-07 12:21:26 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Escape Room Game
+enter the 3-digit code to escape
+fail 3 times, lose the game
+
+Classes-
+GameObject Represents an item the player can interact with
+Room Represents the room we're trying to escape
+Game Represents an instance of  our game that contains the room
+"""
+
+print('Welcome to our escape room!', 'Guess the 3-digit combination to escape!')
+
+name = input('What is your name?\n')
+
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
