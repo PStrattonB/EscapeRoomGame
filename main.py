53394fc905554e1d44ceaaef2e1e28f839d5f87f
strat <sorahbinc@gmail.com>
2023-04-24 03:41:58 -0400

    restarted, might look different than last time. Can't remember.

--- main.py
+++ main.py
@@ -9,17 +9,89 @@ Room Represents the room we're trying to escape
 Game Represents an instance of  our game that contains the room
 """
 
-print('Welcome to our escape room!', 'Guess the 3-digit combination to escape!')
 
-name = input('What is your name?\n')
+class GameObject:
+
+    # Sets up an instance of GameObject with name, appearance, feel, and smell
+    def __init__(self, name, appearance, feel, smell):
+        self.name = name
+        self.appearance = appearance
+        self.feel = feel
+        self.smell = smell
+
+    # Returns string describing object appearance
+    def look(self):
+        return f"You look at the {self.name}. {self.appearance}\n"
+
+    # Returns string describing object feel
+    def touch(self):
+        return f"You touch the {self.name}. {self.feel}\n"
+
+    # Returns string describing object smell
+    def sniff(self):
+        return f"You sniff the {self.name}. {self.smell}\n"
 
 
+class Room:
+    # Our Room class has an escape code and a list of game objects as attributes/fields
+    escape_code = 0
+    game_objects = []
 
-def main():
+    # Initializer
+    def __init__(self, escape_code, game_objects):
+        self.escape_code = escape_code
+        self.game_objects = game_objects
 
+    # Returns whether the code of the room matches the code entered by the player
+    def check_code(self, code):
+        return self.escape_code == code
 
-
-if __name__ == '__main__':
-    main()
+    # Returns a list with all the names of the objects we have in our room
+    def get_game_object_names(self):
+        names = []
+        for object in self.game_objects:
+            names.append(object.name)
+        return names
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+class Game:
+    def __init__(self):
+        self.attempts = 0
+        objects = self.create_objects()
+        self.room = Room(731, objects)
+
+    def create_objects(self):
+        return [
+            GameObject(
+                "Sweater",
+                "It's a blue sweater that had the number 12 switched on it.",
+                "Someone has unstitched the second number, leaving only the 1.",
+                "The sweater smells of laundry detergent."),
+            GameObject(
+                "Chair",
+                "It's a wooden chair with only 3 legs.",
+                "Someone had deliberately snapped off one of the legs.",
+                "It smells like old wood."),
+            GameObject(
+                "Journal",
+                "The final entry states that time should be hours then minutes then seconds (H-M-S).",
+                "The cover is worn and several pages are missing.",
+                "It smells like musty leather."),
+            GameObject(
+                "Bowl of soup",
+                "It appears to be tomato soup.",
+                "It has cooled down to room temperature.",
+                "You detect 7 different herbs and spices."),
+            GameObject(
+                "Clock",
+                "The hour hand is pointing towards the soup, the minute hand towards the chair, and the second hand towards the sweater.",
+                "The battery compartment is open and empty.",
+                "It smells of plastic."),
+            GameObject(
+                "Toy Lightsaber",
+                "It's a toy lightsaber.",
+                "The plastic blade is pitted and bent as if it has seen a number of blade clashes.",
+                "It smells like dust and plastic."),
+        ]
